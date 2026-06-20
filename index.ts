21bf16c76c91fbe94376970bc2e2ac30d0c47070
0xnullsys <2eyesdead@gmail.com>
2026-06-21 01:51:44 +0700

    debug(index): verbose handler logging

--- index.ts
+++ index.ts
@@ -8,11 +8,14 @@ import express from 'express';
 import * as appModule from './server/app.js';
 
 const app: any = (appModule as any).default ?? appModule;
+console.log('[index.ts] app loaded, type=' + typeof app);
 
 export default async function handler(req: any, res: any) {
+  console.log('[handler] ' + req.method + ' ' + req.url);
   try {
     return await new Promise<void>((resolve) => {
       app(req, res, (err: any) => {
+        console.log('[handler] next called err=' + (err?.message ?? 'null'));
         if (err) {
           if (!res.headersSent) {
             res.status(500).json({ error: 'Express', message: String(err?.message ?? err) });
@@ -22,6 +25,7 @@ export default async function handler(req: any, res: any) {
       });
     });
   } catch (err: any) {
+    console.log('[handler] caught ' + err?.message);
     if (!res.headersSent) {
       res.status(500).json({ error: 'Internal', message: String(err?.message ?? err) });
     }
