1cbcd94b37a612d05b45e3a3cc483388f1941b19
0xnullsys <2eyesdead@gmail.com>
2026-06-21 01:20:38 +0700

    fix(index): pass next handler to Express app

--- index.ts
+++ index.ts
@@ -4,5 +4,6 @@ import express from 'express';
 import app from './server/app';
 
 export default function handler(req: any, res: any) {
-  return app(req, res);
+  // Express app is a (req, res, next) function; supply a no-op next so it works as a Vercel handler.
+  return (app as any)(req, res, () => {});
 }
