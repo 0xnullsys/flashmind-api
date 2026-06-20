2d73db3aef3b6a1b9fd651b9058479b117a8192a
0xnullsys <2eyesdead@gmail.com>
2026-06-21 01:19:30 +0700

    fix(index): remove bodyParser override, let Express handle body

--- index.ts
+++ index.ts
@@ -1,14 +1,8 @@
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const config = {};
 
 import express from 'express';
 import app from './server/app';
 
-// Debug log to see what URL Vercel passes
 export default function handler(req: any, res: any) {
-  console.log('[index.ts] method=' + req.method + ' url=' + req.url);
   return app(req, res);
 }
